5ee8647866dd3a0f2d88f3eecc36281c6b879ec5
Nimit Kumar <nimitkumar2002@gmail.com>
2023-03-23 14:44:40 +0530

    Nilesh apna kaam khud karo

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,12 +1,34 @@
 <template>
     <div>
-        <label>Books</label>
+        <v-list dense>
+            <v-list-item v-for="item in books" :key="item._id">
+            <v-list-item-avatar rounded>
+                <v-img :src="item.img"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+
+            <v-list-item-subtitle>{{ item.author.name }}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{ item.pages }}</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+                <v-list-item-subtitle>Status</v-list-item-subtitle>
+            </v-list-item-action>
+        </v-list-item>
+        </v-list>
     </div>
 </template>
 
 <script>
+import booksList from '@/data/books.json'
     export default {
-        
+        data() {
+            return {
+                books: booksList,
+            };
+        },
     }
 </script>
 
